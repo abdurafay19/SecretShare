07db3ff1c16dac535d7619447e7dd704cd245358
Abdul Rafay <abdurafay17wolf@gmail.com>
2026-06-30 18:49:05 +0500

    redis fallback url added

--- backend/app/redis_client.py
+++ backend/app/redis_client.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     decode_responses=True
 )
